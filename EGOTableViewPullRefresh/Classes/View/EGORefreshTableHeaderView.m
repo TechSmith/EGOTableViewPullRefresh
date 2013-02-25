ea3c889b9351ca6bf8ae1e82d2aaf0f7492dac49
Paul Middlin <middlinp@gmail.com>
2013-02-25 11:31:15 -0500

    Dan sez drop shadows are for losers. So. Gone.

--- EGOTableViewPullRefresh/Classes/View/EGORefreshTableHeaderView.m
+++ EGOTableViewPullRefresh/Classes/View/EGORefreshTableHeaderView.m
@@ -57,7 +57,6 @@
 		label.autoresizingMask = UIViewAutoresizingFlexibleWidth;
 		label.font = [UIFont systemFontOfSize:12.0f];
 		label.textColor = textColor;
-		label.shadowColor = [UIColor colorWithWhite:0.9f alpha:1.0f];
 		label.shadowOffset = CGSizeMake(0.0f, 1.0f);
 		label.backgroundColor = [UIColor clearColor];
 		label.textAlignment = UITextAlignmentCenter;
@@ -68,7 +67,6 @@
 		label.autoresizingMask = UIViewAutoresizingFlexibleWidth;
 		label.font = [UIFont boldSystemFontOfSize:13.0f];
 		label.textColor = textColor;
-		label.shadowColor = [UIColor colorWithWhite:0.9f alpha:1.0f];
 		label.shadowOffset = CGSizeMake(0.0f, 1.0f);
 		label.backgroundColor = [UIColor clearColor];
 		label.textAlignment = UITextAlignmentCenter;
